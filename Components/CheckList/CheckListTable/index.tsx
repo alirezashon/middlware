82604ad8d22425a88eb1d6c2bee34ff8cf76f33a
alireza <alirezafeshki2017@gmail.com>
2023-07-01 19:09:07 +0330

    updated not removed todayh, fuck it up

--- Components/CheckList/CheckListTable/index.tsx
+++ Components/CheckList/CheckListTable/index.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useContext, useEffect,useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { CheckedItemsContext } from '../../../Contexts/CheckedItemsContext'
 import { ShowDiagramContext } from '../../../Contexts/DiagramContext'
 import UpdateAsset from '../../UpdateAsset'
@@ -8,83 +8,97 @@ import Modal from '../../Modal'
 import styles from './CheckListTable.module.css'
 import { MdBackspace } from 'react-icons/md'
 interface DataItem {
-  assetCode: string;
-  // Add other properties here
+	assetCode: string
+	// Add other properties here
 }
 
 const CheckListTable: React.FC<{ data: DataItem[] }> = ({ data }) => {
-  const { checkedItems, setCheckedItems } = useContext(CheckedItemsContext);
-  const { showDiagram, setShowDiagram } = useContext(ShowDiagramContext);
-  const [assetCodes, setAssetCodes] = useState<string[]>([]); // New state for asset codes
-  const [selectedItem, setSelectedItem] = useState('')
-//config the setselected , for choose option of status from the modal
-	const handleStatusSelected = (selectedStatus: string) => { 
+	const { checkedItems, setCheckedItems } = useContext(CheckedItemsContext)
+	const { showDiagram, setShowDiagram } = useContext(ShowDiagramContext)
+	const [assetCodes, setAssetCodes] = useState<string[]>([]) // New state for asset codes
+	const [selectedItem, setSelectedItem] = useState('')
+	//config the setselected , for choose option of status from the modal
+	const handleStatusSelected = (selectedStatus: string) => {
 		setSelectedItem(selectedStatus)
 	}
-	
-  useEffect(() => {
-    // Save checkedItems to local storage whenever it changes
-    localStorage.setItem('checkedItems', JSON.stringify(checkedItems));
-  }, [checkedItems]);
 
-  useEffect(() => {
-    // Load checkedItems from local storage on component mount
-    const storedItems = localStorage.getItem('checkedItems');
-    if (storedItems) {
-      setCheckedItems(JSON.parse(storedItems));
-    }
-  }, []);
-console.log(assetCodes)
-  const handleCloseClick = () => {
-    setShowDiagram(false);
-  };
+	useEffect(() => {
+		// Save checkedItems to local storage whenever it changes
+		localStorage.setItem('checkedItems', JSON.stringify(checkedItems))
+	}, [checkedItems])
 
-  const handleCheckboxChange = (assetCode: string) => {
-    const isChecked = checkedItems.some((item) => item.assetCode === assetCode);
-
-    if (isChecked) {
-      setCheckedItems(checkedItems.filter((item) => item.assetCode !== assetCode));
-      setAssetCodes(assetCodes.filter((code) => code !== assetCode)); // Remove asset code from the state
-    } else {
-      const itemToAdd = data.find((item) => item.assetCode === assetCode);
-      if (itemToAdd) {
-        setCheckedItems([...checkedItems, itemToAdd]);
-        setAssetCodes([...assetCodes, assetCode]); // Push asset code into the state
-      }
-    }
-  };
-
-  const handleCheckAllChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const isChecked = event.target.checked;
-
-    if (isChecked) {
-      setCheckedItems(data);
-      setAssetCodes(data.map((item) => item.assetCode)); // Set all asset codes in the state
-    } else {
-      setCheckedItems([]);
-      setAssetCodes([]); // Clear the asset code state
-    }
-  };
-
-  const handleUpdateSubmit = async () => {
-    // Perform your update logic here using the checkedItems state
-    const sampleData = {
-			assetcodes: assetCodes, // Pass the assetCodes state to the API
-			status: 'Mobinnet---Intact_second-hand',
+	useEffect(() => {
+		// Load checkedItems from local storage on component mount
+		const storedItems = localStorage.getItem('checkedItems')
+		if (storedItems) {
+			setCheckedItems(JSON.parse(storedItems))
 		}
-    try {
-      const responses = await UpdateAsset(sampleData);
-      console.log(responses);
-    } catch (error) {
-      console.error(error);
-    }
-    setCheckedItems([]);
-    setAssetCodes([]);
-    handleCloseClick();
-  };
+	}, [])
+	console.log(assetCodes)
+	const handleCloseClick = () => {
+		setShowDiagram(false)
+	}
 
-  // Rest of the component code
-	
+	const handleCheckboxChange = (assetCode: string) => {
+		const isChecked = checkedItems.some((item) => item.assetCode === assetCode)
+
+		if (isChecked) {
+			setCheckedItems(
+				checkedItems.filter((item) => item.assetCode !== assetCode)
+			)
+			setAssetCodes(assetCodes.filter((code) => code !== assetCode)) // Remove asset code from the state
+		} else {
+			const itemToAdd = data.find((item) => item.assetCode === assetCode)
+			if (itemToAdd) {
+				setCheckedItems([...checkedItems, itemToAdd])
+				setAssetCodes([...assetCodes, assetCode]) // Push asset code into the state
+			}
+		}
+	}
+
+	const handleCheckAllChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const isChecked = event.target.checked
+
+		if (isChecked) {
+			setCheckedItems(data)
+			setAssetCodes(data.map((item) => item.assetCode)) // Set all asset codes in the state
+		} else {
+			setCheckedItems([])
+			setAssetCodes([]) // Clear the asset code state
+		}
+	}
+
+	const handleUpdateSubmit = async () => {
+		// Perform your update logic here using the checkedItems state
+		const sampleData = {
+			assetcodes: assetCodes, // Pass the assetCodes state to the API
+			status: selectedItem,
+		}
+		try {
+			const responses = await UpdateAsset(sampleData)
+			console.log(responses)
+			const { completed, failure } = responses
+
+			// Use the completed and failure data as needed
+			console.log('Completed:', completed)
+			console.log('Failure:', failure)
+			const updated = data.map((item) => ({
+				...item,
+				successful: completed.some(
+					(completedItem) => completedItem.assetcode === item.assetCode
+				),
+			}))
+			console.log('Updated:', updated)
+		} catch (error) {
+			console.error(error)
+		}
+		setCheckedItems([])
+		setAssetCodes([])
+		handleCloseClick()
+	}
+
+	// Rest of the component code
+
 	return (
 		<div>
 			{showDiagram && (
@@ -122,6 +136,7 @@ console.log(assetCodes)
 										Object.keys(data[0]).map((key) => <th key={key}>{key}</th>)}
 								</tr>
 							</thead>
+							
 							<tbody>
 								{data.map((row, index) => (
 									<tr key={index}>
@@ -136,7 +151,15 @@ console.log(assetCodes)
 											/>
 										</td>
 										{Object.entries(row).map(([key, value], index) => (
-											<td key={index}>{value}</td>
+											<td
+												key={index}
+												style={
+													key === 'successfull'
+														? { backgroundColor: '#fa7d5a' }
+														: undefined
+												}>
+												{value}
+											</td>
 										))}
 									</tr>
 								))}

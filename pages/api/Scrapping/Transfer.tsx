372ac3ba1ddcbb96c655e6cd536b8d032d29bd83
alireza <alirezafeshki2017@gmail.com>
2023-07-14 22:07:00 +0330

    scrapped

---
+++ pages/api/Scrapping/Transfer.tsx
@@ -0,0 +1,134 @@
+/** @format */
+
+import { NextApiRequest, NextApiResponse } from 'next'
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+
+puppeteer.use(StealthPlugin())
+
+const loginURL = 'http://10.104.26.112/Account/LDAPLogin'
+const targetURL = 'http://10.104.26.112/'
+
+const main = async () => {
+	const browser = await puppeteer.launch({ headless: false }) // Open a visible browser window
+	const page = await browser.newPage()
+	await page.goto(loginURL)
+
+	await page.type('#UserName', 'al.akbari')
+	await page.type('#Password', 'Argon&22')
+	await page.click('[type="submit"]')
+
+	await page.waitForNavigation({ waitUntil: 'networkidle0' })
+	await page.goto(targetURL)
+
+	await page.waitForSelector('.AddPlus')
+	await page.click('.AddPlus')
+
+	await page.waitForSelector('.FormAssetCustomFilterViewCustomSearch')
+	await page.type(
+		'.FormAssetCustomFilterViewCustomSearch',
+		'AST2023336033,AST2022204888,AST2022306419,AST2022306532,AST2022306533,AST2022306547,AST2023333530,AST2023341263,AST2023344271,AST2023340596'
+	)
+	await page.click('.FormAssetCustomFilterViewCustomSearch')
+
+	await page.waitForSelector('.AssetTrackerFileColumn')
+
+	await new Promise((resolve) => setTimeout(resolve, 6000))
+	await page.waitForSelector('.select-checkbox')
+	await page.click('.select-checkbox')
+
+	await page.click('.btn-assettransfer')
+
+	// await page.click('.chosen-search')
+	// await page.type('.chosen-search input', 'Contractor---Intact')
+	await page.waitForTimeout(3000)
+	await page.waitForSelector('.chosen-single')
+	await page.click('#FormAssetCheckTransferFormView_StatusId_chosen')
+
+	await page.waitForSelector('.chosen-single span')
+	await page.type('.chosen-single span', 'Contractor---Intact')
+	await page.keyboard.press('Tab')
+
+	await page.type('#FormAssetCheckTransferFormView_Text_LocationId', 'Tehran')
+	await page.waitForTimeout(3000)
+	await page.keyboard.press('Tab')
+	await page.keyboard.press('Tab')
+	await page.keyboard.press('Tab')
+	await page.keyboard.press('Tab')
+	await page.keyboard.press('Enter')
+	await page.keyboard.press('Enter')
+	await page.keyboard.press('Enter')
+
+	await page.waitForTimeout(3333)
+	
+	await page.click('#AllotedTo_transfer .select2-selection__rendered')
+await page.evaluate(() => {
+	const parentElements = document.querySelectorAll(
+		'#AllotedTo_transfer'
+	)
+
+	parentElements.forEach((parentElement) => {
+		const selectElement = parentElement.querySelector(
+			'select[name="Employee"]'
+		) as HTMLSelectElement | null
+
+		if (selectElement) {
+			selectElement.value = '3107'
+			const event = new Event('change', { bubbles: true })
+			selectElement.dispatchEvent(event)
+		}
+		
+	})
+})
+
+	
+	// await page.evaluate(() => {
+	// 	const selectElement = document.getElementById(
+	// 		'FormAssetTransferFormView_Employee'
+	// 	)!
+
+	// 	// Create a new option element
+	// 	const newOption = document.createElement('option')
+	// 	newOption.value = '3107'
+	// 	newOption.setAttribute('data-select2-id', '47')
+	// 	newOption.textContent = 'Contractor (Contractor@test.ir)'
+
+	// 	// Add the new option element to the select box
+	// 	selectElement.appendChild(newOption)
+	// })
+
+	// await page.type(
+	// 	'#select2-FormAssetTransferFormView_Employee-container',
+	// 	'Contractor (Contractor@test.ir)'
+	// )
+	// await page.click('span[role="presentation"]')
+	// await page.click('div[data-select2-id="10"]')
+	// await page.select(
+	// 	'#FormAssetTransferFormView_Employee',
+	// 	'Contractor (Contractor@test.ir)'
+	// )
+
+    // await page.click('.select2-selection select2-selection--single')
+	// await page.type('.select2-search__field', 'Contractor (Contractor@test.ir)')
+	// await page.keyboard.press('Enter')
+
+	// await page.screenshot()
+}
+
+const waitForTimeout = async (milliseconds: number): Promise<void> => {
+	await new Promise((resolve) => setTimeout(resolve, milliseconds))
+}
+
+const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		await main()
+		await waitForTimeout(5000) // Wait for 5 seconds before responding
+
+		res.status(200).json({ message: 'Action performed successfully' })
+	} catch (error) {
+		console.error('Error performing action:', error)
+		res.status(500).json({ error: 'Error performing action' })
+	}
+}
+
+export default handler
---
+++ pages/api/Scrapping/Update.tsx
@@ -0,0 +1,88 @@
+/** @format */
+
+import { NextApiRequest, NextApiResponse } from 'next'
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+
+puppeteer.use(StealthPlugin())
+
+const loginURL = 'http://asset.mobinnet.net/Account/LDAPLogin'
+const targetURL = 'http://asset.mobinnet.net/'
+
+const main = async () => {
+	const browser = await puppeteer.launch({ headless: false }) // Open a visible browser window
+	const page = await browser.newPage()
+	await page.goto(loginURL)
+
+	await page.type('#UserName', 'al.akbari')
+	await page.type('#Password', 'Argon&22')
+	await page.click('[type="submit"]')
+
+	await page.waitForNavigation({ waitUntil: 'networkidle0' })
+	await page.goto(targetURL)
+
+	await page.waitForSelector('.AddPlus')
+	await page.click('.AddPlus')
+
+	await page.waitForSelector('.FormAssetCustomFilterViewCustomSearch') // Wait for the button action to complete, adjust the timeout value as needed
+	await page.type(
+		'.FormAssetCustomFilterViewCustomSearch',
+		'AST2023336033,AST2022204888,AST2022306419,AST2022306532,AST2022306533,AST2022306547,AST2023333530,AST2023341263,AST2023344271,AST2023340596')
+	await page.click('.FormAssetCustomFilterViewCustomSearch')
+
+	await page.waitForSelector('.AssetTrackerFileColumn')
+
+	
+		await new Promise((resolve) => setTimeout(resolve, 6000))
+		await page.click('.select-checkbox')
+
+	await page.click('.btn-updateasset')
+	await page.waitForTimeout(3000) // Wait for any additional tasks to complete
+
+
+const data = [
+	{ AssetName: 'Akbarpoor', Category: 'Programmer', Brand: 'Islamic-Christ' },
+	{ AssetName: 'Mohammad', Category: 'The Warrior', Brand: 'Islamic' },
+	{ AssetName: 'Chirst', Category: 'Healer', Brand: 'Christian' },
+	{ AssetName: 'Akbarpoor', Category: 'Programmer', Brand: 'Islamic-Christ' },
+	{ AssetName: 'Mohammad', Category: 'The Warrior', Brand: 'Islamic' },
+	{ AssetName: 'Chirst', Category: 'Healer', Brand: 'Christian' },
+	{ AssetName: 'Akbarpoor', Category: 'Programmer', Brand: 'Islamic-Christ' },
+	{ AssetName: 'Mohammad', Category: 'The Warrior', Brand: 'Islamic' },
+	{ AssetName: 'Chirst', Category: 'Healer', Brand: 'Christian' },
+	{ AssetName: 'Akbarpoor', Category: 'Programmer', Brand: 'Islamic-Christ' },
+]
+
+for (let i = 0; i < data.length; i++) {
+	const rowSelector = `tbody tr:nth-child(${i + 1})`
+	const assetCodeCellSelector = `${rowSelector} td:nth-child(3)`
+	const categoryCellSelector = `${rowSelector} td:nth-child(5)`
+	const brandCellSelector = `${rowSelector} td:nth-child(6)`
+
+	await page.click(assetCodeCellSelector)
+	await page.type(assetCodeCellSelector, data[i].AssetName)
+
+	await page.click(categoryCellSelector)
+	await page.type(categoryCellSelector, data[i].Category)
+
+	await page.click(brandCellSelector)
+	await page.type(brandCellSelector, data[i].Brand)
+}
+
+
+
+	
+	//  await browser.close()
+}
+
+const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		const spanTexts = await main()
+		res.status(200).json({ message: 'Action performed successfully' })
+	} catch (error) {
+		console.error('Error performing action:', error)
+		res.status(500).json({ error: 'Error performing action' })
+	}
+}
+
+export default handler
